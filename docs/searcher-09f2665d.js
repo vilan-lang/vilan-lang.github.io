b06808d932b9d66f52b16370dc63a4111e442631
docs-builder <docs@vilan-lang.org>
2026-08-02 19:34:38 +0000

    docs: rebuild the book from vilan-lang/vilan@main

--- docs/searcher-09f2665d.js
+++ docs/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-52f5a681.js',
+                path_to_root + 'searchindex-573f600f.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
